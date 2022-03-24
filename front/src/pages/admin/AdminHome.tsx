15ad1708b58d266043b1ad9bfdd701e9d40ef1cc
tung-dnt <tungdnt779901@gmail.com>
2022-03-24 08:37:39 +0700

    add AdminHome.tsx

---
+++ front/src/pages/admin/AdminHome.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Table, Typography} from 'antd'
+import {useLoading} from '../../context/loadingContext'
+import {Ticket} from '../../types/ticket'
+import {CommonLayout} from '../../components/modules/ComonLayout'
+import {Offer} from '../../types/offer'
+import {User} from '../../types/user'
+import {Spacing} from '../../components/elements/AdminLayout'
+
+type Item = Offer | Ticket | User
+
+const {Title} = Typography
+
+export const AdminHome: React.FC = () => {
+  const [data, setData] = useState<Ticket[]>([])
+  const {loading, setLoading} = useLoading()
+
+  const createColumn = (sampleData: Item, columnWith?: string[]) => {
+    const dataKeys = Object.keys(sampleData)
+    return dataKeys.map((key, index) => (
+      {
+        title: key.toUpperCase(),
+        dataIndex: key,
+        key: key,
+        width: columnWith && columnWith[index],
+        editable: true
+      }
+    ))
+  }
+
+  const fetchTickets = async () => {
+    const response = await axios.get<Ticket[]>(
+      'https://622b018b14ccb950d22be17d.mockapi.io/tickets'
+    )
+    const data = await response.data
+    setData(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchTickets()
+  }, [])
+
+  if (loading) return <>Home Loading...</>
+
+  return (
+    <CommonLayout isAdmin>
+      <Spacing>
+        <Title level={3}>Tickets Management</Title>
+        <Table dataSource={data} columns={createColumn(data[0])}/>
+        <Title level={3}>Offers Management</Title>
+        <Table dataSource={data} columns={createColumn(data[0])}/>
+        <Title level={3}>Users Management</Title>
+        <Table dataSource={data} columns={createColumn(data[0])}/>
+      </Spacing>
+    </CommonLayout>
+  )
+}
