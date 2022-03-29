8dadccff3bbcca74c65662b878298a0b5c98fafc
tung-dnt <tungdnt779901@gmail.com>
2022-03-29 17:32:47 +0700

    add timeout function to ResultPage.tsx

--- front/src/pages/ResultPage.tsx
+++ front/src/pages/ResultPage.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ResultLayout from '../components/pages/result/layout'
 import {useResult} from '../context/searchContext'
 import {LoadingOverlay} from '../components/elements/LoadingOverlay'
 import {useLoading} from '../context/loadingContext'
+import NotFound from './NotFound'
+import {useNavigate} from 'react-router-dom'
 
 const ResultPage: React.FC = () => {
+  const navigate = useNavigate()
   const {loading} = useLoading()
   const {resultTickets} = useResult()
+  const [isTimeOut, setIsTimeOut] = useState<boolean>(false)
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsTimeOut(true)
+    }, 10000)
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/')
+    }, 14000)
+  }, [isTimeOut])
+
+  if (isTimeOut) return <NotFound/>
   if (loading) return <LoadingOverlay/>
 
   return (
