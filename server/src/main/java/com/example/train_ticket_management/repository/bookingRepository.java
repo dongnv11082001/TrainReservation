21a6bf37122a2a4f72b3e07ecdfaab775caf7c70
dongnv11082001 <hiiamgosu4@gmail.com>
2022-03-29 11:57:56 +0700

    refactor

--- server/src/main/java/com/example/train_ticket_management/repository/bookingRepository.java
+++ server/src/main/java/com/example/train_ticket_management/repository/bookingRepository.java
@@ -1,7 +1,7 @@
 package com.example.train_ticket_management.repository;
 
-import com.example.train_ticket_management.entity.Ticket;
+import com.example.train_ticket_management.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface bookingRepository extends JpaRepository<Ticket, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
