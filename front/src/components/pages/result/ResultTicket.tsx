8b05f3c13b90aa93abd27ceaebfbcdce05d4b52c
tung-dnt <tungdnt779901@gmail.com>
2022-03-31 13:43:33 +0700

    refactor date display issue

--- front/src/components/pages/result/ResultTicket.tsx
+++ front/src/components/pages/result/ResultTicket.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import moment from 'moment'
 import {useNavigate} from 'react-router-dom'
 import {Ticket} from '../../../types/ticket'
 import {useResult} from '../../../context/searchContext'
@@ -18,6 +19,7 @@ export const ResultTicket: React.FC<ResultTicketProps> = ({ticket}) => {
   const {contextRoundTrip} = useResult()
   const {inCartTickets, setInCartTickets} = useCartTickets()
   const {setLoading} = useLoading()
+  const dateFormat = 'DD/MM/YYYY'
 
   const handleSelect = () => {
     setLoading(true)
@@ -37,7 +39,9 @@ export const ResultTicket: React.FC<ResultTicketProps> = ({ticket}) => {
   return (
     <TicketWrapper>
       <div>
-        <TextSize20>{ticket.departureTime} <SwapRightOutlined /> {ticket.arrivalTime}</TextSize20>
+        <TextSize20>{moment(ticket.departureTime).format(dateFormat)}
+          <SwapRightOutlined/> {moment(ticket.arrivalTime).format(dateFormat)}
+        </TextSize20>
         <div>{ticket.destination}</div>
         <ClassAndPriceWrapper>
           <div>
@@ -47,7 +51,7 @@ export const ResultTicket: React.FC<ResultTicketProps> = ({ticket}) => {
         </ClassAndPriceWrapper>
         <ButtonWrapper>
           <Link to={'/'}>More ticket information</Link>
-          <Button onClick={handleSelect}>Buy</Button>
+          <Button onClick={handleSelect}>Choose</Button>
         </ButtonWrapper>
       </div>
     </TicketWrapper>
@@ -88,8 +92,8 @@ const ButtonWrapper = styled.div`
 const Button = styled.button`
   cursor: pointer;
   padding: 4px 16px; 
-  background-color: #325a50; 
-  color: #fff; 
+  background-color: #d1e1b7; 
+  color: #325a50; 
   border: 0; 
   border-radius: 4px;
   
